9feb33999c34c019e805cbaf8433e471542852fd
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2019-04-04 08:35:23 -0400

    updated the event logger to only send one event at a time (since we talking hours between msg)

--- src/HGV.Nullifer.Service/EventLogger.cs
+++ src/HGV.Nullifer.Service/EventLogger.cs
@@ -11,40 +11,25 @@ namespace HGV.Nullifer.Service
     public class EventLogger : ILogger
     {
         private EventLog events;
-        private List<string> messages;
 
         public EventLogger(EventLog e)
         {
-            this.messages = new List<string>();
             this.events = e;
         }
 
         public void Error(Exception ex)
         {
-            this.Flush(true);
             this.events.WriteEntry(ex.Message, EventLogEntryType.Error);
         }
 
         public void Warning(string msg)
         {
-            this.messages.Add(msg);
-            this.Flush(false);
+            this.events.WriteEntry(msg);
         }
 
         public void Info(string msg)
         {
-            this.messages.Add(msg);
-            this.Flush(false);
-        }
-     
-        private void Flush(bool now = false)
-        {
-            if(this.messages.Count >= 10 || now)
-            {
-                var entry = string.Join(Environment.NewLine, this.messages.ToArray());
-                this.events.WriteEntry(entry);
-                this.messages.Clear();
-            }
+            this.events.WriteEntry(msg);
         }
     }
 }
